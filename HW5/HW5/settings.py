8829232192b3e9f857ea2f8c1f1826c942458053
SN <SN@gmail.com>
2024-05-11 10:51:18 +0300

    second

--- HW5/HW5/settings.py
+++ HW5/HW5/settings.py
@@ -22,12 +22,19 @@ MEDIA_URL = '/media/'
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3z^c$7-h&2@hmdi)gjtgt_ow(do_jkw-zjbl8sh%4rc74aq2-n'
+
+import os
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [
+    'ASN1986.pythonanywhere.com',
+]
 
 
 # Application definition
@@ -78,8 +85,15 @@ WSGI_APPLICATION = 'HW5.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ASN1986$default',
+        'USER': 'ASN1986',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'ASN1986.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
@@ -119,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
